5d131199577bda662c8dddf044d23c49ffad08e2
chuayingweidexter <Dex_Chua83@hotmail.com>
2022-10-13 17:09:06 +0800

    .

--- 3204-php/dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect("127.18.0.24","root","abc123","3204_db");
-
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
