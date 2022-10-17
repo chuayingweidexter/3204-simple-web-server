8373331452fffded63a1e2b8ec42bbc4fba47576
Chua Ying Wei Dexter <72144948+chuayingweidexter@users.noreply.github.com>
2022-10-18 00:05:26 +0800

    Update home.php

--- 3204-php/home.php
+++ 3204-php/home.php
@@ -2,9 +2,6 @@
 include('dbcon.php');
 include('session.php'); 
 
-$result=mysqli_query($con, "select * from Accounts where User_id='$session_id'")or die('Error In Session');
-$row=mysqli_fetch_array($result);
-
  ?>
 
 <html>
@@ -13,15 +10,19 @@ $row=mysqli_fetch_array($result);
 </head>
 <body>
 <div class="form-wrapper"> 
-    <center><h3>Welcome: <?php echo $row['Username']; ?> </h3></center>
+    <center><h3>Welcome</h3></center>
 	 <div class="reminder">
     <?php
+    $sql ="SELECT * FROM [SSH Credentials] WHERE Accounts_User_id LIKE '%$search%'";
+		print "<p> $sql </p>";
+		print "<hr>";
+		$result = mysqli_query($conn, $sql);
 		$queryResult = mysqli_num_rows($result);
     if($queryResult > 0){
         while($row = mysqli_fetch_assoc($result)){
           echo "<tr><td>".$row["IP_Address"]."</td>";
-                  echo "<td>".$row["Username"]."</td>";
-                  echo "<td>".$row["Password"]."</td>";
+          echo "<td>".$row["Username"]."</td>";
+          echo "<td>".$row["Password"]."</td>";
         }
       }
     ?>
