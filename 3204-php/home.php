fdac2132e36702370cfde4c963e839c16e4ee9b2
Chua Ying Wei Dexter <72144948+chuayingweidexter@users.noreply.github.com>
2022-10-17 23:56:24 +0800

    Update home.php

--- 3204-php/home.php
+++ 3204-php/home.php
@@ -15,6 +15,16 @@ $row=mysqli_fetch_array($result);
 <div class="form-wrapper"> 
     <center><h3>Welcome: <?php echo $row['Username']; ?> </h3></center>
 	 <div class="reminder">
+    <?php
+		$queryResult = mysqli_num_rows($result);
+    if($queryResult > 0){
+        while($row = mysqli_fetch_assoc($result)){
+          echo "<tr><td>".$row["IP_Address"]."</td>";
+                  echo "<td>".$row["Username"]."</td>";
+                  echo "<td>".$row["Password"]."</td>";
+        }
+      }
+    ?>
     <p><a href="logout.php">Log out</a></p>
   </div>
 </div>
