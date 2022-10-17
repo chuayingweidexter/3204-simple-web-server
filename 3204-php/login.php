7074d35d494119e3ba21d7ba04008dd5297c5333
Chua Ying Wei Dexter <72144948+chuayingweidexter@users.noreply.github.com>
2022-10-18 00:48:57 +0800

    Update login.php

--- 3204-php/login.php
+++ 3204-php/login.php
@@ -29,12 +29,12 @@
 			$password = $_POST['pass'];
 			
 			$query 		= mysqli_query($con, "SELECT * FROM Accounts WHERE Password='$password' and Username='$username'");
-			$row		= mysqli_fetch_all($query);
+			$row		= mysqli_fetch_array($query);
 			$num_row 	= mysqli_num_rows($query);
 			
 			if ($num_row > 0) 
 				{			
-					$_SESSION['user_id']=$row['User_id'];
+					$_SESSION['user_id']=$username;
 					header('location:home.php');
 					
 				}
