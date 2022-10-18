c2d784841b14c6962009a25567c75bc8bedfa094
gteckseng <51436245+gteckseng@users.noreply.github.com>
2022-10-18 21:54:04 +0800

    Update upload.php

--- 3204-php/upload.php
+++ 3204-php/upload.php
@@ -1,17 +1,26 @@
-<form enctype="multipart/form-data" action="uploader.php" method="POST">
-<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
-Choose a file to upload: <input name="uploadedfile" type="file" /><br />
-<input type="submit" value="Upload File" />
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
 </form>
 
+</body>
+</html>
+
 <?php
-$target_path="uploads/";
+$target_path = "uploads/";
 //Here we set the target path that will save the file in to.
-$target_path = $target_path.basename($_FILES['uploadedfile']['name']);
+$target_path = $target_path . basename($_FILES['uploadedfile']['name']);
 // here wll move the desired file from the tmp directory to the target path
-if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'],$target_path)){
-echo "the file " . basename($_FILES['uploadedfile']['name']) . " has been uploaded! ";
-}else {
-echo "there was an error uploading the file ,please try again!";
+if (move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+    echo "the file " . basename($_FILES['uploadedfile']['name']) . " has been uploaded! ";
+} else {
+     echo "there was an error uploading the file ,please try again!";
 }
 ?>
+
