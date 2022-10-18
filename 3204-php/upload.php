595db5daf9c984a8a96aea2c3d966afae489106b
gteckseng <51436245+gteckseng@users.noreply.github.com>
2022-10-18 20:57:55 +0800

    Update upload.php

--- 3204-php/upload.php
+++ 3204-php/upload.php
@@ -1,4 +1,4 @@
-<form enctype="multipart/form-data" action="upload.php" method="POST">
+<form enctype="multipart/form-data" action="uploader.php" method="POST">
 <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
 Choose a file to upload: <input name="uploadedfile" type="file" /><br />
 <input type="submit" value="Upload File" />
